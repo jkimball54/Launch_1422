51af1341237b2ab5005cba1d98318a8e3049b1e2
jkimball54 <jeremyck54@gmail.com>
2023-08-03 10:37:54 -0600

    User story 7/8 complete

--- CaddyShackMVC/Controllers/GolfBagsController.cs
+++ CaddyShackMVC/Controllers/GolfBagsController.cs
@@ -51,5 +51,28 @@ namespace CaddyShackMVC.Controllers
 
             return RedirectToAction("show", new { id = golfbag.Id});
         }
+
+        [Route("golfbags/{id:int}/edit")]
+        public IActionResult Edit(int id)
+        {
+            var golfBag = _context.GolfBags.Include(g => g.Clubs).Where(g => g.Id == id).FirstOrDefault();
+            return View(golfBag);
+        }
+
+        [HttpPost]
+        [Route("golfbags/{id:int}")]
+        public IActionResult Update(int id, Club club)
+        {
+            club.GolfBag = _context.GolfBags.Find(id);
+            club.GolfBagId = _context.GolfBags.Find(id).Id;
+            //Had an issue with club not having a unique id
+            //so I came up with this workaround
+            club.Id = _context.Clubs.Select(c => c.Id).Max() + 1;
+            //might be some tech debt, but it works as is
+            _context.Clubs.Add(club);
+            _context.SaveChanges();
+
+            return RedirectToAction("show", new { id = club.GolfBag.Id });
+        }
     }
 }
