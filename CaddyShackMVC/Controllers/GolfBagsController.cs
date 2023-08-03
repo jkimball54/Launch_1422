58e4a2776d034250f1af487abbd227a902f63608
jkimball54 <jeremyck54@gmail.com>
2023-08-03 09:13:40 -0600

    User story 1 complete

---
+++ CaddyShackMVC/Controllers/GolfBagsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CaddyShackMVC.DataAccess;
+
+namespace CaddyShackMVC.Controllers
+{
+    public class GolfBagsController : Controller
+    {
+        private readonly CaddyShackContext _context;
+
+        public GolfBagsController(CaddyShackContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var golfBags = _context.GolfBags.ToList();
+            return View(golfBags);
+        }
+    }
+}
